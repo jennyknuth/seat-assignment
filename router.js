88fac66e4ddf554ce1eb3986fb609de6bfbe0c28
Jenny Knuth <inkboco@gmail.com>
2015-07-12 20:31:31 -0600

    files set up, npm installed, home and public routes working

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 var routes = require('routes')(),
     fs = require('fs'),
     qs = require('qs'),
-    db = require('monk')('localhost/travelblog'),
+    db = require('monk')('localhost/seating'),
     seats = db.get('seats'),
     users = db.get('users'),
     view = require('./view'),
@@ -12,11 +12,23 @@ routes.addRoute('/', function (req, res, url) {
   var email = req.session.get('email')
   if (req.method === 'GET') {
     res.setHeader('Content-Type', 'text/html')
-    var template = view.render('index', )
+    seats.find({}, function (err, docs) {
+      if (err) res.end('no files found')
+      var template = view.render('sessions/index', {seats: docs})
+      res.end(template)
+    })
   }
 })
-
-
-
-
+routes.addRoute('/public/*', function (req, res, url) {
+  if (req.method === 'GET') {
+    res.setHeader('Content-Type', mime.lookup(req.url))
+    fs.readFile('.' + req.url, function (err, file) {
+      if (err) {
+        res.setHeader('Content-Type', 'text/html')
+        res.end('404')
+      }
+      res.end(file)
+    })
+  }
+})
 module.exports = routes
